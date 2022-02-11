94fad0cd10a8a9a98f8ac2f9a79342dcae056e1f
sergeedouard <sjoseph.eng@gmail.com>
2022-02-11 15:46:07 -0500

    Both leet_code problems complete

--- tabulation_project/lib/leet_code_64.js
+++ tabulation_project/lib/leet_code_64.js
@@ -2,6 +2,11 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid,x = 0, y=0) {
+    // When assessing the recursive tree, I found that each node formula would be cost(i,j)=grid[i][j]+min(cost(i+1,j),cost(i,j+1))
+    // The base case is the bottom right cell
+    // This time complexity is O(2^(m * n)). This is because for every move, we have atmost 2 options
+    // Space complexity: O(m+n). Recursion of depth m+n
+    
     // If the x or y value of the grid exceed the parameters of the 2D array, return a Max number. This will be so that the recursive call doesn't select this option
     if (x === grid.length || y === grid[0].length) return Number.MAX_VALUE
     // When drawing the tree, the bottom right cell is the base case. Once you reach that base case, we want to return the value at that cell
@@ -15,3 +20,25 @@ grid2 = [[1,2,3],[4,5,6]] // Output 12
 
 console.log(minPathSum(grid1))
 console.log(minPathSum(grid2))
+
+//Tabulation
+// This would be the preferred solution due to the lower complexity
+function minPathSum(grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+    let table = new Array(m).fill().map(() => new Array(n).fill(Infinity));
+    table[0][0] = grid[0][0];
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i < m - 1) {
+                table[i + 1][j] = Math.min(table[i][j] + grid[i + 1][j], table[i + 1][j]);
+            }
+            if (j < n - 1) {
+                table[i][j + 1] = Math.min(table[i][j] + grid[i][j + 1], table[i][j + 1]);
+            }
+        }
+    }
+
+    return table[m - 1][n - 1];
+}
--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -1,6 +1,30 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function climbStairs(n) {
+function climbStairsRecursion(n,memo = {}) {
+    // Recursive first.
+    // Base case would be when there are no more steps lefts
+    // We must also keep in mind that the amount of steps cannot be less than 0 so in those cases, we'll return 0 since it is not a valid step
+    if (n === 0) return 1;
+    if (n < 0) return 0;
 
+    // Because so many of the nodes are recurring, it would be beneficial memoize the function
+    if (n in memo) return memo[n];
+
+    // This is the recursive formula
+    memo[n] = climbStairsRecursion(n-2, memo) + climbStairsRecursion(n-1, memo);
+    return memo[n]
+}
+
+//Tabulation Method
+function climbStairs(n) {
+    let table = new Array(n + 1);
+    table[0] = 1;
+    table[1] = 1;
+
+    for (let i = 2; i < table.length; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[table.length - 1];
 }
