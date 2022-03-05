3c004fbbd4604a8b00263149ec73af16613d8b63
sergeedouard <sjoseph.eng@gmail.com>
2022-03-04 19:00:58 -0500

    Added continue feature when the first element of the right side is greater than the last element of the left side. This makes the best case scenerio more efficient

--- Sorting Algos/insertion_sort_project/lib/insertion_sort.js
+++ Sorting Algos/insertion_sort_project/lib/insertion_sort.js
@@ -2,8 +2,13 @@ function insertionSort(arr) {
     // Must iterate from the second element. 
     for(let i = 1; i < arr.length; i++) {
         
+        // Since the left side is assumed to be sorted, if the first element of the right side is already greater than the last element of the left side,
+        // Continue to the next number
+        if (arr[i] > arr[i-1]) continue;
+
         // The nested loop will iterate through the sorted side.
         // Starting with the first element of the unsorted side, it will compare the unsorted element with through the sorted side
+
         for(let j = i - 1; j >= 0; j--) {
             if (arr[j] >= arr[j+1] ) {
                 //whenever the sorted side has a larger value, the two values will be swapped
