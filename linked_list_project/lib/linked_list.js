93cd352507ae271b418f5fb6f9e708b22ee9b555
sergeedouard <sjoseph.eng@gmail.com>
2022-03-08 13:14:32 -0500

    Completed methods for linked lists

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -94,42 +94,142 @@ class LinkedList {
 
     // TODO: Implement the addToHead method here
     addToHead(val) {
+        // Create a new node
+        let addHead = new Node(val);
 
+        // Make the new node next reference the current head. Thus making it the new head in practice
+        addHead.next = this.head;
+        
+        // within the list, official make the new head the new node.
+        this.head = addHead;
+        
+        // If the list doesn't originally have any nodes, then the added node will be the head and the tail.
+        if (this.length === 0) {
+            this.tail = addHead;
+        }
+
+        // increment the length attribute
+        this.length++;
+        return this
     }
 
     // TODO: Implement the removeHead method here
     removeHead() {
+        // If the list is already empty, there are no nodes to remove, therefor the action is undefined
+        if (!this.length) return undefined
 
+        // The following is just a placeholder for the head that will be removed
+        let removedHead = this.head;
+
+        // The two scenarios are if the linked list only has 1 node, the tail and head should be reassigned to null since the list will now be empty
+        // Otherwise, the list tail will not be influenced so we would just need to reassign the head.
+        if (this.length === 1) {
+            this.tail = null;
+            this.head = null;
+        } else {
+            this.head = this.head.next;
+        }
+        // Decrease the length count
+        this.length--;
+
+        // Return the removed node
+        return removedHead;
     }
 
     // TODO: Implement the contains method here
     contains(target) {
+        // If the list is empty it will definitely not contain the target
+        if (!this.length) return false
 
+        // The current will start at the head
+        let current = this.head
+
+        // Iterate through each node until the target is found
+        while(current) {
+            if (current.value === target) return true
+            current = current.next;
+        }
+
+        // If the target isn't found, return false
+        return false;
     }
 
     // TODO: Implement the get method here
     get(index) {
+        // Return null if the index is out of bounds
+        if (index > this.length - 1 || index < 0) return null
 
+        let current = this.head;
+
+        //Iterate through the nodes until the index is reached
+        for (let node = 0; node < index; node++) {
+            current = current.next;
+        }
+
+        // return the node at the desired index
+        return current;
     }
 
     // TODO: Implement the set method here
     set(index, val) {
+        // Return false if the index is out of bounds
+        if (index > this.length - 1 || index < 0) return false
 
+        let current = this.head;
+        let node = 0;
+
+        // While the current node exists, return true is the node is at the desired index. Otherwise, incremented the node value and the move to the next node.
+        while (current) {
+            if (node === index) {
+                current.value = val;
+                return true
+            } else {
+                current = current.next;
+                node++
+            }
+        }
     }
 
     // TODO: Implement the insert method here
     insert(index, val) {
+        if (index >= this.length) return false;
+        let addNode = new Node(val);
 
+        
+        let afterNode = this.get(index)
+
+        if (this.length > 1) {
+            let preNode = this.get(index - 1);
+            preNode.next = addNode;
+        } else {
+            this.head = addNode;
+        }
+        
+        addNode.next = afterNode;
+        this.length++;
+        return true
     }
 
     // TODO: Implement the remove method here
     remove(index) {
+        if (!this.head) return undefined;
+        if (index >= this.length || index < 0) return undefined;
+        if (index === 0) return this.removeHead();
+        if (index === this.length - 1) return this.removeTail();
+        
+        let removedNode = this.get(index);
+        let preNode = this.get(index - 1);
+        let afterNode = this.get(index + 1);
+
+        preNode.next = afterNode;
+        this.length--;
+        return removedNode;
 
     }
 
     // TODO: Implement the size method here
     size() {
-
+       return this.length;
     }
 }
 
