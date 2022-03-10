1a3621dc79e013e7ef97ff57c9cfbf8ad511ab5f
sergeedouard <sjoseph.eng@gmail.com>
2022-03-09 19:24:57 -0500

    Queue Exercises

---
+++ queue_project/lib/queue.js
@@ -0,0 +1,73 @@
+// ============================================================================
+// Implementation Exercise: Queue
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Queue and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let node = new Node(val);
+
+        if (!this.length) {
+            this.front = node;
+            this.back = node;
+        } else {
+            this.back.next = node;
+            this.back = node;
+        }
+
+        this.length++
+        return this.length;
+    }
+
+    dequeue() {
+        if (!this.length) return null;
+
+        let processed = this.front;
+        if (this.front === this.back) {
+            this.front = null;
+            this.back = null;
+        } else {
+            this.front = processed.next; 
+        }
+        
+        this.length--;
+        return processed.value;
+    }
+
+    size() {
+        return this.length;
+    }
+}
+
+exports.Node = Node;
+exports.Queue = Queue;
