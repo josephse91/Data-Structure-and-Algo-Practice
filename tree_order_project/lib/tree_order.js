5f6aa8ef16b2065eb0b34720fce25cb5be0bdc06
sergeedouard <sjoseph.eng@gmail.com>
2022-03-10 19:13:02 -0500

    binary trees

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,9 +1,26 @@
 function inOrderArray(root) {
+    if (!root) return [];
+
+    let array = [];
+
+    return [ 
+        ...inOrderArray(root.left), 
+        root.val, 
+        ...inOrderArray(root.right) 
+    ];
 
 }
 
 function postOrderArray(root) {
+    if (!root) return [];
 
+    let array = [];
+
+    return [
+        ...postOrderArray(root.left),
+        ...postOrderArray(root.right),
+        root.val,
+    ];
 }
 
 
