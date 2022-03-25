a2d49540f60acbb137b23145dbedc46354ba099e
sergeedouard <sjoseph.eng@gmail.com>
2022-03-25 11:31:38 -0400

    leetcode problems 1 & 366

---
+++ leetcode/1.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/two-sum/
+
+let twoSum = function(nums, target) {
+    let pairs = {};
+    // /*
+    nums.forEach(function(el) { 
+        if (pairs[target - el]) {
+            return [nums.indexOf(el), nums.indexOf(target-el)];
+        } else {
+            pairs[el] = true;
+        }
+    })
+// */
+/*
+    for (let i = 0; i < nums.length; i++) {
+        if (pairs[target - nums[i]]) {
+            return [i, nums.indexOf(target-nums[i])]
+        } else {
+            pairs[nums[i]] = true;
+        }
+    }
+    */
+    return null;
+};
+
+nums1 = [2,7,11,15]
+target1 = 9
+// [0,1]
+nums2 = [3,2,4]
+target2 = 6
+// [1,2]
+nums3 = [3,3]
+target3 = 6
+// [0,1]
+
+console.log(twoSum(nums1,target1))
+console.log(twoSum(nums2,target2))
+console.log(twoSum(nums3,target3))
