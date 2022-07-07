9f0dfa908d1d15ddf6679d21deea279cccbfdc59
Serge <92275998+josephse91@users.noreply.github.com>
2022-07-07 15:47:19 -0400

    Time: 79 ms (92.11%), Space: 43.8 MB (47.37%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.js
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.js
@@ -28,20 +28,20 @@ var findMinFibonacciNumbers = function(k) {
     }
     return count
     
-    /*
-    const minFib = function(num,arr) {
-        if (num === 0) return 1;
-        if (num < 0) return Infinity;
-        
-        let paths = []
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > num) continue;
-            let nextNum = arr[i];
-            paths.push(minFib(num - nextNum,arr))
-            // console.log(paths)
-        }
-        return 1 + Math.min(...paths);
-    }
-    return (minFib(k,fibNums) -1)
-    */
 };
+
+/* Attempt 1
+I had tried to attempt this using an exhaustive recursive strategy. This would take the given k and subtract each of the fib values equal to or below the k value and within the recursive tree, find the minimum path.
+
+There were two problems:
+1) The process took too much time beyond a trivially normal number like 20
+2) The answer I found wasn't correct for each number
+    
+
+
+*/
+
+/* Attempt 2
+
+I had to acknowledge that the pattern of the fibonacci meant that I could subtract the biggest available fib number from k and there will be a divisible under it that will be the most efficient path. This typically is the case with any problem like this that has a 1 within its options or if all of the numbers are divisible within each other
+*/
