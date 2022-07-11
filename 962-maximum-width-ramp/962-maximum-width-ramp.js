471c653d8b87a547169e1693a7ca1dfa31f889c1
Serge <92275998+josephse91@users.noreply.github.com>
2022-07-11 14:24:07 -0400

    Time: 2498 ms (34.61%), Space: 44.9 MB (84.62%) - LeetHub

---
+++ 962-maximum-width-ramp/962-maximum-width-ramp.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxWidthRamp = function(nums) {
+    let maxRamp = 0;
+    
+    for (let i = 0; i < nums.length;i++) {
+        if (nums.length - i - 1 < maxRamp) break;
+        let j = nums.length - 1;
+        let currentRamp = 0;
+        
+        while (!currentRamp && j > i) {
+            if (nums[j] - nums[i] >= 0) currentRamp = j-i;
+            j--
+        }
+        
+        if (currentRamp > maxRamp) maxRamp = currentRamp;
+    }
+    return maxRamp;
+};
+
+//two pointer method. Once second pointer finds a valid value, break.
+// Once the first pointer gets to the element minus the current maxWidth break the loop
