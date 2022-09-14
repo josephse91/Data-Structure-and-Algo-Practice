3ec9e863c00569510904463de84938dd2bb592b7
Serge <92275998+josephse91@users.noreply.github.com>
2022-09-14 11:41:18 -0400

    Time: 184 ms (31.83%), Space: 49.7 MB (37.24%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -4,27 +4,17 @@
  */
 var merge = function(intervals) {
     intervals.sort((a,b) => a[0] - b[0]);
-    let start = [];
-    let end = [];
     let output = [];
     
-    for (let [currStart,currEnd] of intervals) {
-        start.push(currStart);
-        end.push(currEnd);
-    }
-    
-    for (let i = 1; i <= start.length; i++) {
-        if (i < start.length && start[i] <= end[i - 1]) {
-            start[i] = Math.min(start[i], start[i-1]);
-            end[i] = Math.max(end[i], end[i-1]);
+    for (let i = 1; i <= intervals.length; i++) {
+        
+        if (i < intervals.length && intervals[i][0] <= intervals[i - 1][1]) {
+            intervals[i][0] = Math.min(intervals[i][0], intervals[i-1][0]);
+            intervals[i][1] = Math.max(intervals[i][1], intervals[i-1][1]);
         } else {
-            output.push([start[i-1],end[i-1]])
+            output.push([intervals[i-1][0],intervals[i-1][1]])
         }
     }
-    
-    
-    
-    
     
     return output;
 };
