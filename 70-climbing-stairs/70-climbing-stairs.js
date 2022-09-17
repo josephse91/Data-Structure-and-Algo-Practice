8ab62fdc126d742d8796e192863410bda617791c
Serge <92275998+josephse91@users.noreply.github.com>
2022-09-16 21:15:03 -0400

    Time: 46 ms (99.93%), Space: 41.9 MB (56.45%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,6 +2,23 @@
  * @param {number} n
  * @return {number}
  */
+
+var climbStairs = function(n,memo = {}) {
+    let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2
+    
+    for (let i = 3; i < dp.length;i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    return dp[n]
+    
+};
+
+
+/*
+
 var climbStairs = function(n,memo = {}) {
     if (n === 1) return 1;
     if (n === 2) return 2;
@@ -10,3 +27,4 @@ var climbStairs = function(n,memo = {}) {
     memo[n] = climbStairs(n - 1,memo) + climbStairs(n-2,memo) 
     return memo[n]
 };
+*/
