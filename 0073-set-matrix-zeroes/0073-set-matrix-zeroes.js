6bd4c5b830cf63e91fd41100e43cb57dedd71a80
Serge <92275998+josephse91@users.noreply.github.com>
2022-10-11 16:56:19 -0400

    Time: 171 ms (12.18%), Space: 44.2 MB (97.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -2,6 +2,32 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
+var setZeroes = function(matrix) {
+    let change = new Map();
+    change.set("rows",new Set());
+    change.set("cols",new Set());
+    
+    for (let run = 1; run <= 2; run++) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[0].length; j++) {
+                if (!matrix[i][j] && run === 1) {
+                    change.get("rows").add(i);
+                    change.get("cols").add(j);
+                }
+
+                if ((change.get("rows").has(i) || change.get("cols").has(j)) 
+                    && run === 2) {
+                    matrix[i][j] = 0;
+                }
+
+            }
+        }
+    }
+    
+}
+
+/*
 var setZeroes = function(matrix) {
     let queue = [];
     
@@ -28,3 +54,4 @@ var setZeroes = function(matrix) {
         }
     }
 };
+*/
