711095ad3d0cf7687a835073ed105afd58612e50
Serge <92275998+josephse91@users.noreply.github.com>
2022-10-22 15:40:08 -0400

    Time: 732 ms (66.23%), Space: 110.7 MB (84.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -5,6 +5,34 @@
  * @param {number} destination
  * @return {boolean}
  */
+
+var validPath = function(n,edges,source,destination){
+    if (!edges.length) return true
+    let graph = {};
+    
+    for (let [edge1,edge2] of edges) {
+        if (!graph[edge1]) graph[edge1] = new Set();
+        if (!graph[edge2]) graph[edge2] = new Set();
+        
+        graph[edge1].add(edge2);
+        graph[edge2].add(edge1);
+    }
+    
+    let curr = source;
+    let queue = [curr], visited = new Set();
+    
+    while(queue.length) {
+        curr = queue.shift();
+        
+        visited.add(curr);
+        graph[curr].forEach(neighbors => {
+            if (!visited.has(neighbors)) queue.push(neighbors)
+        })
+    }
+    return visited.has(destination)
+}
+
+/*
 var validPath = function(n, edges, source, destination) {
     let bucket = new Array(n).fill(false);
     bucket[source] = true;
@@ -24,3 +52,4 @@ var validPath = function(n, edges, source, destination) {
     }
     return bucket[destination]
 };
+*/
